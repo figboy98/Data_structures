0517756971c74b68636c766914eb23f6a4c96433
figboy98 <f.m.g.silva@hotmail.com>
2019-03-04 22:29:57 +0000

    Palindromo

--- Pratica2/Palindromo.java
+++ Pratica2/Palindromo.java
@@ -1,24 +1,54 @@
 import java.util.Scanner;
+import java.lang.Object;
+import java.lang.String;
 
 public class Palindromo{
 	
-	public static void Normaliza(char v[]){ //32 a 48
-		v=toLowerCase(v);
+	public static String Normaliza(String frase){ //32 a 48
+		int cont=0;
+		frase=frase.toLowerCase();
+		frase=frase.replaceAll("[^a-z]", "");
 		
-		
-		for(int i=0; i<v.length; i++){
-			System.out.print(v[i]);
-		}
+		return frase;
 	}
 	
+	public static boolean isPalindrome(String frase){
+		int size=frase.length();
+		
+		for(int i=0; i<frase.length(); i++){
+			if(frase.charAt(i) != frase.charAt(size-1)){
+				return false;
+			}
+			size--;
+		}
+		return true;
+	}
 	
 	public static void main( String [] args){
 		
 		Scanner stdin = new Scanner(System.in);
 		
-		int v= stdin.hasNextLine();
+		int size=stdin.nextInt();
 		
-		Normaliza(v);
+		System.out.println(size);
+		
+		stdin.nextLine();
+		
+		for(int i=0; i<size; i++){
+		
+		String frase=stdin.nextLine();
+		
+		String novaFrase=Normaliza(frase);
+		
+		if(isPalindrome(novaFrase)){
+			System.out.println("sim");
+		}
+		
+		else{
+			System.out.println("nao");
+			}
+		}
+		
 	}
 }
 			
