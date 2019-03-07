2a8bf46e7e8afc429191e753c8899e6ad5e1addd
figboy98 <f.m.g.silva@hotmail.com>
2019-03-07 21:41:53 +0000

    Cardinais

--- Pratica2/Cardinais.java
+++ Pratica2/Cardinais.java
@@ -1,44 +1,54 @@
 import java.util.Scanner;
-public class Cardinais{
-	
-	public static char[][] lerMatriz(){
-		
-		
-		Scanner stdin= new Scanner(System.in);
-		
-		int L= stdin.nextInt();
-		int C= stdin.nextInt();
-		
-		stdin.nextLine();
-		
-		char[][] matriz = new char[L][C];
 
-	for(int i = 0; i < L; i++){
-		String line = stdin.nextLine();
-			for(int j = 0; i < C; j++){
-				matriz[i][j] = line.charAt(j);
-			}
-		}	
-		
-	for(int i=0; i<L; i++){
-		for(int j=0; j<C; j++){
-			System.out.println(matriz[i][j]);
-		}
-	}
-		
-	
-	return matriz;
-	
+
+class Matrix {
+   char data[][]; // os elementos da matriz em si
+   int rows;     // numero de linhas
+   int cols;     // numero de colunas
+
+   // construtor padrao de matriz
+   Matrix(int r, int c) {
+      data = new char[r][c];
+      rows = r;
+      cols = c;
+   }
+
+   // Ler os rows x cols elementos da matriz
+   public void read(Scanner in) {
+      for (int i=0; i<rows; i++)
+         for (int j=0; j<cols; j++){
+			 char c = in.next().charAt(j);
+			data[i][j] = c;
+   }
 }
-	
-		
-	
+
+   // Representacao em String da matrix
+   public String toString() {
+      String ans = "";
+      for (int i=0; i<rows; i++) {
+         for (int j=0; j<cols; j++)
+            ans += data[i][j];
+         ans += "\n";
+      }
+      return ans;
+   }
+}
+
+public class Cardinais{
+
 	
 	public static void main(String []args){
+		Scanner stdin= new Scanner(System.in);
+		int linhas= stdin.nextInt();
+		int colunas= stdin.nextInt();
+		Matrix cardinais= new Matrix(linhas,colunas);
+		//char[][] = new char[rlinhas[colunas];
 		
-		char aux[][]= new char[2][2];
 		
-		lerMatriz();
+		cardinais.read(stdin);
+		System.out.println();
+		System.out.println(cardinais);
+		
 		
 		
 	}
