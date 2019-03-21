34bfe5005c9386fecbc9f9c0ea80b6d7a7a54c73
figboy98 <f.m.g.silva@hotmail.com>
2019-03-21 10:08:49 +0000

    Pratica 5

---
+++ Pratica5/Unique.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Unique{
+	
+	public static void main(String []args){
+	
+	Scanner in = new Scanner(System.in);
+	
+	IntSet s = new MyIntSet();
+	
+	while(in.hasNextInt()){
+		
+		s.add(in.nextInt());
+	}
+	
+	System.out.println(x.size());
+}
+}
+
+		 
