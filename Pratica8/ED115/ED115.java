3f7afc19d357884eb08b9aae05b58a411927bc64
figboy98 <f.m.g.silva@hotmail.com>
2019-05-11 20:26:20 +0100

    ED115

--- Pratica8/ED115/ED115.java
+++ Pratica8/ED115/ED115.java
@@ -21,6 +21,7 @@ class Caixas{
 	int totalClientes;
 	int totalProdutos;
 	int tempoSaida;
+	int produtosUltimo;
 	
 	//Construtor
 	Caixas(int a){
@@ -28,18 +29,21 @@ class Caixas{
 		atendimento=a;
 		totalClientes=0;
 		totalProdutos=0;
+		tempoSaida=0;
 	}
 	
 	void novoCliente(Cliente a){
 		fila.addLast(a);
 		totalClientes++;
 		totalProdutos+=a.produtos;
-		tempoSaida= a.tempSaida;	
+		tempoSaida=a.tempSaida;	
+		produtosUltimo=a.produtos;
 }
 }
 public class ED115{
 	private static Caixas listaCaixas[];//array com as Caixas existentes
 	private static MyQueue<Cliente> filaClientes= new LinkedListQueue<Cliente>(); //Queue com os clientes que ainda não estão alocados a nenhuma caixa
+	private static Cliente atendidos[]; //array com clientes atendidos
 	private static int numCaixas;
 	private static int numClientes;
 	private static int clock=0;
@@ -56,24 +60,28 @@ public class ED115{
 			filaClientes.enqueue(aux);
 			in.nextLine();
 		}
+		
+		atendidos= new Cliente[numClientes];
 	}
 		
 	private static void lerCaixas(Scanner in){
 			listaCaixas= new Caixas[numCaixas];
+			
 			for(int i=0; i<numCaixas; i++){
 				int atendimento= in.nextInt();
 				Caixas aux= new Caixas(atendimento);
 				
 				listaCaixas[i]=aux;
+				//System.out.println(listaCaixas[i].atendimento);
 			}
+			
 			in.nextLine();
 		}
 	
 private static void mostraClientes(){
 	
 	for(int i=0; i<numClientes; i++){
-		Cliente aux=listaCaixas[0].fila.getFirst();
-		listaCaixas[0].fila.removeFirst();
+		Cliente aux=atendidos[i];
 		System.out.println(aux.nome +" "+ aux.tempChegada +" " + aux.tempSaida);
 	}
 }
@@ -96,12 +104,59 @@ private static int tempEspera(int index, Cliente aux){
 }
 
 private static void processarClientes(){
+	for(int i=0; i<numClientes; i++){
+		Cliente aux=filaClientes.dequeue();
+		clock=aux.tempChegada;
+		int index=escolheCaixa();
+		atualizaCliente(aux, index);
+		listaCaixas[index].novoCliente(aux);
+		atendidos[i]=aux;	
+	}
 }
-		
+private static int escolheCaixa(){
+	
+	for(int i=0; i<numCaixas; i++){
+		Cliente aux=listaCaixas[i].fila.getFirst();
+		if(aux!=null){
+			if(aux.tempSaida < clock){
+				listaCaixas[i].fila.removeFirst();
+			}
+		}
+	}
+	
+	int best=listaCaixas[0].fila.size();
+	int index=0;
+	
+	for(int i=0; i<numCaixas; i++){
+		if(listaCaixas[i].fila.isEmpty()){
+			index=i;
+			return index;
+		} 
+		else if(listaCaixas[i].fila.size() < best){
+			best=listaCaixas[i].fila.size();
+			
+			index=i;
+		}
+		else if(listaCaixas[i].fila.size() == best){ //Situação de empate
+			int aux1=listaCaixas[index].produtosUltimo;
+			int aux2=listaCaixas[i].produtosUltimo;
+			if(aux2 < aux1) index =i;
+			if(aux2==aux1){
+			if(i<index) index=i;
+			}
+		}
+	}
+	
+	return index;
+}
 
+private static void atualizaCliente(Cliente aux, int index){
+	int processamento= (listaCaixas[index].atendimento)*aux.produtos +10;
+	int espera=listaCaixas[index].tempoSaida - aux.tempChegada;
+	if(espera <0) espera=0;
 
-private static int escolheCaixa(\){
-	return 0;
+	int total=processamento+clock+ espera;
+	aux.tempSaida=total;
 }
 
 		
